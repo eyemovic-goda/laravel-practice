e9b0012f095f88f855ab38926a1ea9cdf54bb944
合田和樹 <goda@eyemovic.com>
2020-04-30 22:18:44 +0900

    DBのセレクト文を学習

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -5,18 +5,22 @@ namespace App\Http\Controllers;
 use App\Http\Requests\HelloRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Validator;
 
 class HelloController extends Controller
 {
     public function index(Request $request)
     {
-        $msg = "cookieは存在しません。";
-        if ($request->hasCookie("msg")) {
-            $msg = "cookie:" . $request->cookie("msg");
+        if (isset($request->id)) {
+            $param = ["id" => $request->id];
+            $items = DB::select("select * from people where id = :id",
+                $param);
+        } else {
+            $items = DB::select('select * from people');
         }
 
-        return view("hello.index", ["msg" => $msg]);
+        return view("hello.index", ["items" => $items]);
     }
 
     public function post(HelloRequest $request)
