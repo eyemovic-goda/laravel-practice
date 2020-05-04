85598415fe6b62bcf92a5dba84caea27fff43899
合田和樹 <goda@eyemovic.com>
2020-05-04 13:43:35 +0900

    seederの利用

---
+++ database/seeds/PeopleTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PeopleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $params = [
+            [
+                "name" => "taro",
+                "mail" => "taro@yamada.jp",
+                "age" => 12,
+            ],
+            [
+                "name" => "hanako",
+                "mail" => "hanako@flower.jp",
+                "age" => 34,
+            ], [
+                "name" => "sachiko",
+                "mail" => "sachiko@happy.jp",
+                "age" => 56,
+            ],
+        ];
+        DB::table("people")->insert($params);
+    }
+}
